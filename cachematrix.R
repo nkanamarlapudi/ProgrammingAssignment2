25dbfe978d46198241c11de7e9608c6ad7be539b
Naresh <naresh.kkumar@gmail.com>
2017-09-04 00:18:15 -0400

    Changes made as per Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <<- solve
+  if(!is.null(m)){
+        message("getting cahced data")
+        return(m)
+  }
+  data <- makeCacheMatrix(x)
+  m <- solve(data)
+  m
 }
